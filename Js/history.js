8b8149a45811ac4b566d2bea7c95e31450d05390
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-09-16 10:05:34 +0600

    project done

---
+++ Js/history.js
@@ -0,0 +1,48 @@
+const histories = document.getElementById("histories");
+
+function addHistory(questionText, timeTaken, errorCount, wpm) {
+
+  const newRow = document.createElement("div");
+  newRow.classList.add("card");
+
+  newRow.innerHTML = `
+  <h3>${questionText}</h3>
+  <div>
+  <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+  <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+  </div>
+  `;
+
+  histories.appendChild(newRow);
+
+  let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+  previousTests.push({ questionText, timeTaken, errorCount, wpm });
+  localStorage.setItem("testHistory", JSON.stringify(previousTests));
+
+  displayHistory();
+}
+
+function displayHistory() {
+  histories.innerHTML = "";
+  const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+  console.log(previousTests);
+  // null notifi
+  if (previousTests.length === 0) {
+    console.log("inside if");
+    histories.innerHTML = `<h2 style="color:white; text-align: center; opacity:.7">No histories available</h2>`
+  }
+
+  previousTests.forEach((test) => {
+    const newRow = document.createElement("div");
+    newRow.classList.add("card");
+
+    newRow.innerHTML = `
+  <h3>${test.questionText}</h3>
+  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+    <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+    <p>WPM: <span class="bold greenish-white">${test.wpm}</span></p>
+  `;
+
+    histories.appendChild(newRow);
+  });
+}
